f9ac1f0bb7b4befa78d93ebbb2bcf1650a195486
fengw <1401617948@qq.com>
2016-04-05 10:25:09 +0800

    Http Client

--- AndroidOnTheWay/src/com/demo/androidontheway/http/HttpClient.java
+++ AndroidOnTheWay/src/com/demo/androidontheway/http/HttpClient.java
@@ -6,9 +6,14 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
 import com.wafejlu.uiproductionfactory.R;
 
-import android.R.integer;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
@@ -21,6 +26,7 @@ import android.widget.TextView;
 public class HttpClient extends Activity implements OnClickListener {
 
 	public static final int SHOW_RESPONSE = 0;
+	private Button btnHttpURLConnection;
 	private Button btnHttpClient;
 	private TextView tvHttpClient;
 
@@ -30,8 +36,10 @@ public class HttpClient extends Activity implements OnClickListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.http_client_activity);
 
+		btnHttpURLConnection = (Button) findViewById(R.id.btn_http_url_connection_test);
 		btnHttpClient = (Button) findViewById(R.id.btn_http_client_test);
 		tvHttpClient = (TextView) findViewById(R.id.tv_http_client);
+		btnHttpURLConnection.setOnClickListener(this);
 		btnHttpClient.setOnClickListener(this);
 	}
 
@@ -49,6 +57,31 @@ public class HttpClient extends Activity implements OnClickListener {
 			}
 		};
 	};
+
+	private void sendRequestWithHttpClient() {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					DefaultHttpClient httpClient = new DefaultHttpClient();
+					HttpGet httpGet = new HttpGet("http://www.baidu.com");
+					HttpResponse httpResponse = httpClient.execute(httpGet);
+					if (httpResponse.getStatusLine().getStatusCode() == 200) {
+						// 请求和响应都成功了
+						HttpEntity entity = httpResponse.getEntity();
+						String response = EntityUtils.toString(entity, "utf-8");
+						Message message = new Message();
+						message.what = SHOW_RESPONSE;
+						// 将服务器返回的结果存放到Message中
+						message.obj = response.toString();
+						handler.sendMessage(message);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+	}
 
 	private void sendRequestWithHttpURLConnection() {
 		// 开启线程来发起网络请求
@@ -91,8 +124,11 @@ public class HttpClient extends Activity implements OnClickListener {
 	public void onClick(View arg0) {
 		// TODO Auto-generated method stub
 		switch (arg0.getId()) {
-		case R.id.btn_http_client_test:
+		case R.id.btn_http_url_connection_test:
 			sendRequestWithHttpURLConnection();
+			break;
+		case R.id.btn_http_client_test:
+			sendRequestWithHttpClient();
 			break;
 
 		default:
