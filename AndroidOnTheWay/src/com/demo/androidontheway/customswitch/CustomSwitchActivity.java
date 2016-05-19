fa981c890051933fd6a3255a26ad1bbf28000880
wangfeng <wangfeng@localhost.com>
2016-05-19 11:44:02 +0800

    add Tint color;IOS Style;Disable State;Onclick Toast

--- AndroidOnTheWay/src/com/demo/androidontheway/customswitch/CustomSwitchActivity.java
+++ AndroidOnTheWay/src/com/demo/androidontheway/customswitch/CustomSwitchActivity.java
@@ -4,13 +4,31 @@ import com.demo.androidontheway.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class CustomSwitchActivity extends Activity {
 
+	private SwitchButton mToastSwitchButton = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.custom_switch_activity);
+		
+		mToastSwitchButton = (SwitchButton) findViewById(R.id.sb_onclick_toast);
+		mToastSwitchButton.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton arg0, boolean arg1) {
+				// TODO Auto-generated method stub
+				if (arg1) {
+					Toast.makeText(CustomSwitchActivity.this, "Check ON", Toast.LENGTH_SHORT).show();
+				}else {
+					Toast.makeText(CustomSwitchActivity.this, "Check OFF", Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
 	}
 }
